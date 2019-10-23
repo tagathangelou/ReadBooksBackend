179a0ad0028fa4302b159d1a97f5123c8e5c37a8
tagathangelou <tagathangelou@yahoo.com>
2019-10-23 09:52:52 +0300

    Update file  - added Posts table

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobil
 
 mobileApp.tables.add('Book'); // Create a table for 'Book' with default settings
 mobileApp.tables.add('Users');
+mobileApp.tables.add('Users');
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
